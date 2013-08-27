547cbd359b861c87b732dad2f49905bf6ac9521e
Derik Strattan <derik.strattan@gmail.com>
2013-08-26 17:24:39 -0700

    Added the Contact link on #Home which links directly to Contacts page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 CfWeekThree::Application.routes.draw do
-  get "pages/home"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -8,6 +8,7 @@ CfWeekThree::Application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
